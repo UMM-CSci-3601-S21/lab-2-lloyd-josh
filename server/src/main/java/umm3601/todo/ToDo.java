c624c95e968854c7a6612a367e2f15675fae2dfd
Josh Quist <quist127@morris.umn.edu>
2021-02-11 13:01:08 -0600

    Fix ToDo variables
    Fixing ToDo variables so that the client successfully prints out all ToDo items.

--- server/src/main/java/umm3601/todo/ToDo.java
+++ server/src/main/java/umm3601/todo/ToDo.java
@@ -3,8 +3,8 @@ package umm3601.todo;
 public class ToDo {
 
   public String _id;
-  public String name;
-  public int age;
+  public String owner;
+  public boolean status;
+  public String body;
   public String company;
-  public String email;
 }
