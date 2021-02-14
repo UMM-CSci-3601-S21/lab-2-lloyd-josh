d085010591a56b8c461fc3f7c1dde1088a5295f9
Josh Quist <quist127@morris.umn.edu>
2021-02-13 19:42:36 -0600

    Add test for limiting getToDos()
    Added a test for the limit filter, which fails if the limit is a string instead of an integer.

--- server/src/test/java/umm3601/todo/ToDoControllerSpec.java
+++ server/src/test/java/umm3601/todo/ToDoControllerSpec.java
@@ -12,7 +12,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import io.javalin.core.validation.Validator;
+import io.javalin.http.BadRequestResponse;
 import io.javalin.http.Context;
 import io.javalin.http.NotFoundResponse;
 
@@ -61,6 +67,20 @@ public class ToDoControllerSpec {
     when(ctx.pathParam("id", String.class)).thenReturn(new Validator<String>("nonexistent", "", "id"));
     Assertions.assertThrows(NotFoundResponse.class, () -> {
       todoController.getToDo(ctx);
+    });
+  }
+
+  @Test
+  public void GET_to_request_todos_with_illegal_limit() {
+    // testing entering a limit with a string, which is illegal
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put("limit", Arrays.asList(new String[] { "limit" }));
+
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+    // This should now throw a `BadRequestResponse` exception because
+    // our request has a limit that can't be parsed to a number.
+    Assertions.assertThrows(BadRequestResponse.class, () -> {
+      todoController.getToDos(ctx);
     });
   }
 }
